681276f2b2c3cdcfac9d9db5c25785aeb17ccf1b
emrekgn <emre.akkaya@agem.com.tr>
2016-08-16 11:19:02 +0300

    update script to generate unsigned deb

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -19,7 +19,7 @@ echo "lider & lider-console modules built successfully."
 # Generate Ahenk package
 echo "Generating Ahenk package..."
 cd "$PRJ_ROOT_PATH"/ahenk-script
-dpkg-buildpackage -b
+dpkg-buildpackage -b -uc
 echo "Generated Ahenk package"
 
 EXPORT_PATH=/tmp/lider-ahenk-script-plugin
