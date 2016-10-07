72e3f919c05b0a2abd84bb220875ff09c1410af0
Volkan Şahin <bm.volkansahin@gmail.com>
2016-10-07 11:14:09 +0300

    main module info added

--- ahenk-script/script/main.py
+++ ahenk-script/script/main.py
@@ -3,4 +3,14 @@
 
 
 def info():
-    return None
+    inf = dict()
+    inf['name'] = 'script'
+    inf['version'] = '1.0.0'
+    inf['support'] = 'debian'
+    inf['description'] = ''
+    inf['task'] = True
+    inf['user_oriented'] = False
+    inf['machine_oriented'] = False
+    inf['developer'] = ''
+
+    return inf
